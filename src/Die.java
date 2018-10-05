3c9d41a7c42065789b201c63f3c305c7a9bbd7b9
idking <idking@BusElab12.BUSINESS.ColoState.EDU>
2018-10-05 10:59:00 -0600

    First commit: set up IAW UML diagram

---
+++ src/Die.java
@@ -0,0 +1,46 @@
+//***********************************************************************
+//*																		*
+//* CIS611					Fall 2018									*
+//*																		*
+//*						Program Assignment PP2							*
+//*																		*
+//*						Dice simulation game							*
+//*																		*
+//*						Created 05 Oct 2018								*
+//*																		*
+//*						Saved in RunDice.java							*
+//*																		*
+//***********************************************************************
+
+
+public class Die {
+
+//class variables****************************
+	private int number;
+	private boolean keep;
+	
+//constructor********************************
+	public Die() {
+		
+	}
+	
+//get set methods**************************************
+	public boolean getKeep() {
+		return keep;
+	}
+	
+	public void setKeep(boolean newKeepValue) {
+		keep = newKeepValue;
+	}
+	
+	public int getNumber() {
+		return number;
+	}
+	
+	public void setNumber(int newNumber) {
+		number = newNumber;
+	}
+
+//behavioral methods*************************************
+	
+}
---
+++ src/RunDice.java
@@ -0,0 +1,56 @@
+//***********************************************************************
+//*																		*
+//* CIS611					Fall 2018									*
+//*																		*
+//*						Program Assignment PP2							*
+//*																		*
+//*						Dice simulation game							*
+//*																		*
+//*						Created 05 Oct 2018								*
+//*																		*
+//*						Saved in RunDice.java							*
+//*																		*
+//***********************************************************************
+
+
+
+public class RunDice {
+
+//class variables ************************************
+	static int rollNum;
+	static boolean six;
+	static boolean five;
+	static boolean four;
+	
+//constructor
+	public RunDice() {
+		//fill this in
+	}
+	
+	
+	public static void main(String[] args) {
+		
+		
+
+	}//end main*********************************************
+
+//behavioral methods****************************************
+	public static void roll() {
+		
+	}
+	
+	public static void initDie() {
+		
+	}
+	
+	public static int calculateScore() {
+		int theScore = 0;
+		
+		return theScore;
+	}
+	
+	public static void finalScore() {
+		
+	}
+
+}//end class************************************************
